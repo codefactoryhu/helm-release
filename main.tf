19f2bf5e75b2a39f6f83fbabe17e9041bffd9870
Sós Gellért <gellertsos@gmail.com>
2025-10-01 17:45:40 +0200

    update variables to be able to deploy multiple charts

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 resource "helm_release" "this" {
-  name             = var.name
-  repository       = var.repository
-  chart            = var.chart
-  version          = var.chart_version
-  create_namespace = var.create_namespace
-  namespace        = var.kubernetes_namespace
+  for_each = var.helm_releases
+
+  name             = each.key
+  repository       = each.value.repository
+  chart            = each.value.chart
+  version          = each.value.chart_version
+  create_namespace = each.value.create_namespace
+  namespace        = each.value.kubernetes_namespace
+
+  atomic          = true
+  cleanup_on_fail = true
+
+  # it only works if the locals.tags input is a map
+  values = lookup(each.value, "tags", null) != null ? [
+    yamlencode({ tags = each.value.tags })
+  ] : []
 }
