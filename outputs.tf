c69996aacae6edba0dba42c07e2614a358dcac09
Sós Gellért <gellertsos@gmail.com>
2025-10-01 17:50:00 +0200

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
-output "release_name" {
-  value       = helm_release.this.name
-  description = "The name of the deployed Helm release."
+output "release_names" {
+  description = "The names of all deployed Helm releases"
+  value       = [for r in helm_release.this : r.name]
 }
 
-output "namespace" {
-  value       = helm_release.this.namespace
-  description = "The namespace where the Helm release was installed."
+output "release_charts" {
+  description = "The chart used for each deployed Helm release"
+  value       = { for k, r in helm_release.this : k => r.chart }
 }
 
-output "chart" {
-  value       = helm_release.this.chart
-  description = "The chart deployed by this Helm release."
+output "release_namespaces" {
+  description = "The namespace of each deployed Helm release."
+  value       = { for k, r in helm_release.this : k => r.namespace }
+}
+
+output "release_values" {
+  description = "The computed values for each Helm release."
+  value       = { for k, r in helm_release.this : k => r.values }
+}
+
+output "release_statuses" {
+  description = "The status of each deployed Helm release."
+  value       = { for k, r in helm_release.this : k => r.status }
 }
